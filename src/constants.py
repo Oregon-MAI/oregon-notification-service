bab95d37fcea55d9e079bbc50269e7a2054c8d22
Leonid Gurianov <leonid.11521@gmail.com>
2026-04-28 21:07:24 +0300

    delete postgres

--- src/constants.py
+++ src/constants.py
@@ -17,4 +17,3 @@ CONSUME_TOPICS = [
 ]
 KAFKA_BOOTSTRAP_SERVERS = os.getenv("KAFKA_BOOTSTRAP_SERVERS")
 KAFKA_GROUP_ID = "notification"
-DB_URL: str = os.getenv("DATABASE_URL", "")
--- src/repositories/message_repository.py
+++ src/repositories/message_repository.py
@@ -25,31 +25,3 @@ async def delete_messages_by_user_id(user_id: UUID) -> None:
     keys = [key async for key in r.scan_iter(f"{user_id}:*")]
     if keys:
         await r.delete(*keys)
-
-
-"""
-engine = create_async_engine(DB_URL, echo=True, future=True)
-
-async_session = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
-
-async def get_message(id: UUID) -> Message:
-    async with async_session() as session, session.begin():
-        result = await session.execute(select(Message).where(Message.id == id).options())
-        return result.scalar_one()
-
-
-async def get_messages_by_user_id(user_id: UUID) -> list[Message]:
-    async with async_session() as session, session.begin():
-        result = await session.execute(select(Message).where(Message.user_id == user_id))
-        return list(result.scalars().all())
-
-
-async def insert_message(new_message: Message) -> None:
-    async with async_session() as session, session.begin():
-        session.add(new_message)
-
-
-async def delete_messages_by_user_id(user_id: UUID) -> None:
-    async with async_session() as session, session.begin():
-        await session.execute(delete(Message).where(Message.user_id == user_id))
-"""
